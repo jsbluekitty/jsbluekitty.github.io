5f331dae9f284bf3a65d77de8cf96473be4de6af
Wu404 <327067661@qq.com>
2020-03-23 10:23:59 +0800

    Site updated: 2020-03-23 10:23:59

--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
           var data = res.list[j].arr;
           var liTmpl = "";
           for (var i = 0, len = data.link.length; i < len; i++) {
-            var minSrc = 'https://github.com/jsbluekitty/BlogBackup/tree/master/min_photos' + data.link[i];
-            var src = 'https://github.com/jsbluekitty/BlogBackup/tree/master/photos' + data.link[i];
+            var minSrc = 'https://raw.githubusercontent.com/jsbluekitty/BlogBackup/master/min_photos/' + data.link[i];
+            var src = 'https://raw.githubusercontent.com/jsbluekitty/BlogBackup/master/photos/' + data.link[i];
             var type = data.type[i];
             var target = src + (type === 'video' ? '.mp4' : '.jpg');
             src += '';
